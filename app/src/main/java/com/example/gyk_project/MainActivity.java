f5350f0a6fac48c29da373258d9b0e8c03380b98
bilaloz <152805013@ogr.cbu.edu.tr>
2019-03-17 11:20:17 +0300

    GYK ODev

--- app/src/main/java/com/example/gyk_project/MainActivity.java
+++ app/src/main/java/com/example/gyk_project/MainActivity.java
@@ -3,42 +3,39 @@ package com.example.gyk_project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AndroidException;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGirisYap, btnKayitOl;
-    EditText edtKullaniciAdi, edtSifre;
-    ImageView imgLogo;
+
+    ListView listSehirler;
+
+    String [] sehirler = {"İstanbul","Ankara","İzmir","Adana","Konya","Manisa","Karabük","Ağrı"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnGirisYap = findViewById(R.id.btnGirisYap);
-        btnKayitOl = findViewById(R.id.btnKayitOl);
-        edtKullaniciAdi = findViewById(R.id.kullaniciAdi);
-        edtSifre = findViewById(R.id.kullaniciSifre);
-        imgLogo = findViewById(R.id.logo);
+        listSehirler = findViewById(R.id.listSehirler);
 
-        btnGirisYap.setOnClickListener(new View.OnClickListener() {
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,android.R.id.text1,sehirler);
+        listSehirler.setAdapter(arrayAdapter);
+        listSehirler.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-               if (edtKullaniciAdi.equals("Bilal") && edtSifre.equals("1234")){
-                   Intent i = new Intent(MainActivity.this, AnasayfaActivity.class);
-                   i.putExtra("name", edtKullaniciAdi.getText());
-                   i.putExtra("password", edtSifre.getText());
-
-                   startActivity(i);
-
-               }else {
-                   Toast.makeText(MainActivity.this, "Kullanıcı adınız veya şifreniz yanlış!", Toast.LENGTH_SHORT).show();
-               }
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        "TIklanılan yer = "+sehirler[position],
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
